a103c18e597e0e7faf4f6099fb1f757b3f5c546e
imtoophat <jasonhchern@gmail.com>
2018-12-02 14:30:57 -0500

    tags class addition

---
+++ tags.php
@@ -0,0 +1,10 @@
+<?php
+
+class Tags {
+
+	public $tag_id;
+	public $tag_name;
+	public $aggr_time_to_do;
+}
+
+?>
--- to_do_items.php
+++ to_do_items.php
@@ -2,8 +2,18 @@
 
 class To_Do_Items {
 
-	public $items;
-	public $
+	public $to_do;
+	public $to_do_name;
+	public $time_taken;
+	public $start_time;
+	public $end_time;
+
+	public $user_id;
+	public $tag_id;
+
+	
+
+
 }
 
 ?>
