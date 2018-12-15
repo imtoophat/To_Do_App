8fabecc972859cc902ef1e8a62a3b98e47071e87
byang6 <42845512+byang6@users.noreply.github.com>
2018-12-14 19:09:42 -0500

    Update database.php

--- php/database.php
+++ php/database.php
@@ -7,9 +7,4 @@ $username = 'root';
 $password = '';
 $database = 'todo_db'; //enter name of the table to work with
 
-$conn = new mysqli($hostname, $username, $password, $database);
-if ($conn->connect_errno) {
-  die('ERROR NO DATABSE');
-}
-
 ?>
