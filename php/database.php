0ad3cac428a16187ce0f73b20161a4b9262fb8ff
byang6 <42845512+byang6@users.noreply.github.com>
2018-12-14 19:06:04 -0500

    Update database.php

--- php/database.php
+++ php/database.php
@@ -2,17 +2,14 @@
 
 //default configs for connecting to the database
 
-$hostname = 'ENTER IP';
+$hostname = 'localhost';
 $username = 'root';
-$password = 'snake477';
-$database = 'organizations'; //enter name of the table to work with
+$password = '';
+$database = 'todo_db'; //enter name of the table to work with
 
 $conn = new mysqli($hostname, $username, $password, $database);
 if ($conn->connect_errno) {
   die('ERROR NO DATABSE');
 }
-
-//test to see if index can reach this file
-//echo "HELLO";
 
 ?>
